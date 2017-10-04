f606633bbac87a930b8e60b13347020302310846
Bruno Pulis <bruno.pulis@vianetbrasil.com.br>
2017-10-04 19:25:42 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const API = 'https://got-quotes.herokuapp.com/quotes';
+
+fetch(API)  
+  .then((response) => {
+    response.json().then( (data) => {
+      console.log(data)
+      
+      let phrase    = document.querySelector('#phrase')
+      let character = document.querySelector('#character')
+      
+      phrase.innerHTML    = data.quote
+      character.innerHTML = data.character
+    })
+  })
+  .catch(function(err){
+    console.error('Failed retrieving information', err)
+  })
